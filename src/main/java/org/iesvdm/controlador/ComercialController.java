20900fd3c67ff2d843b8a10262063d8ecab3fe45
Carmen <cmardom599@g.educaand.es>
2024-01-11 12:05:22 +0100

    listar comerciales corregido y terminado

--- src/main/java/org/iesvdm/controlador/ComercialController.java
+++ src/main/java/org/iesvdm/controlador/ComercialController.java
@@ -3,17 +3,19 @@ package org.iesvdm.controlador;
 import org.iesvdm.modelo.Comercial;
 import org.iesvdm.service.ComercialService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
-
+@Controller
 public class ComercialController {
 
+    @Autowired
     private ComercialService comercialService;
 
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-    @Autowired //FALLO EN AUTOWIRED
+    //@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired 
     public ComercialController (ComercialService comercialService) {this.comercialService = comercialService;}
 
     @GetMapping("/comerciales")
